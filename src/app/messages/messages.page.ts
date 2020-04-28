21ea806ad31c9109c736c785f4a6a4ea24b608ab
badlam624 <blakeadlam@gmail.com>
2020-04-28 15:10:39 -0700

    adding messaging

--- src/app/messages/messages.page.ts
+++ src/app/messages/messages.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-messages',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.page.scss'],
 })
 export class MessagesPage implements OnInit {
-
-  constructor() { }
+  messages = [];
+  constructor(
+    
+    ) {}
 
   ngOnInit() {
+    var email = firebase.auth().currentUser.email;
+    var db = firebase.firestore();
+    db.collection("messages").where("recipient",'==',email).get().then((snapshot) =>{snapshot.docs.forEach(doc => {
+      var item = doc.data();
+      
+      //Add Task to Project in Project Service
+      this.messages.push(item);
+
+    })
+    });
+
+    
   }
+
+  upload(message, recipient) {
+    var db = firebase.firestore();
+    var user = firebase.auth().currentUser.email;
+    db.collection("messages").add({
+      'recipient':recipient,
+      'sender':user,
+      "message":message,
+    })
+    .then(function(docRef) {
+       console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+      console.error("Error adding document: ", error);
+    });
+  }
+
+
 
 }
