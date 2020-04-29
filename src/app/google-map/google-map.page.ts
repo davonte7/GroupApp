1981ef7c807a09737bcbd9bd0f75fc6dffbf2fe6
Vishwajeet Singh <vs2@email.sc.edu>
2020-04-29 00:40:50 -0400

    Removing these

--- src/app/google-map/google-map.page.ts
+++ src/app/google-map/google-map.page.ts
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Geolocation} from '@ionic-native/geolocation/ngx';
 declare var google;
-=======
-import { Component, OnInit } from '@angular/core';
-import { Geolocation } from '@ionic-native/geolocation/ngx'
-import { ActivatedRoute, Router } from '@angular/router';
-import { MeetingsDetailsPage } from '../meetings-details/meetings-details.page';
-
-declare var google: any;
-
->>>>>>> ec9f847320c6192e940faccb146485244b365a69
 @Component({
   selector: 'app-google-map',
   templateUrl: './google-map.page.html',
   styleUrls: ['./google-map.page.scss'],
 })
 
-<<<<<<< HEAD
 export class GoogleMapPage implements OnInit, AfterViewInit {
   @ViewChild('nativeElement', {static: false}) mapNativeElement: ElementRef;
   directionsService = new google.maps.DirectionsService;
@@ -30,16 +19,6 @@ export class GoogleMapPage implements OnInit, AfterViewInit {
   };
   constructor(private fb: FormBuilder, private geolocation: Geolocation) {
     this.createDirectionForm();
-=======
-export class GoogleMapPage implements OnInit{
-  map:any;
-  currentProject: any;
-  constructor(
-    private geo: Geolocation,
-    private route: ActivatedRoute,
-    private router: Router
-  ) {
->>>>>>> ec9f847320c6192e940faccb146485244b365a69
   }
 
   ngOnInit() {
@@ -51,7 +30,6 @@ export class GoogleMapPage implements OnInit{
     });
   }
 
-<<<<<<< HEAD
   ngAfterViewInit(): void {
     this.geolocation.getCurrentPosition().then((resp) => {
       this.currentLocation.lat = resp.coords.latitude;
@@ -78,27 +56,5 @@ export class GoogleMapPage implements OnInit{
       }
     });
   }
-=======
-      var marker = new google.maps.Marker({
-        position: {
-          lat: res.coords.latitude, lng: res.coords.longitude
-        },
-        map: this.map
-      });
-    }).catch( e => {
-      console.log(e);
-    })
-
-    this.route.params.subscribe(
-      param => {
-        this.currentProject = param;
-  })
-    
-  }
-  
-  goBack(){
-    this.router.navigate(["project-detail",this.currentProject]);
-  }
->>>>>>> ec9f847320c6192e940faccb146485244b365a69
 
 }
