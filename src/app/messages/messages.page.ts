fcc17cdf06a6be7862a0b72937df3bb5e46214c1
badlam624 <blakeadlam@gmail.com>
2020-04-29 17:11:33 -0700

    tweaked messaging

--- src/app/messages/messages.page.ts
+++ src/app/messages/messages.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProjectService } from '../services/project.service';
+import { Subscriber, Subscription } from 'rxjs';
 import * as firebase from 'firebase';
 
 @Component({
@@ -7,15 +10,25 @@ import * as firebase from 'firebase';
   styleUrls: ['./messages.page.scss'],
 })
 export class MessagesPage implements OnInit {
+  currentProject:any;
   messages = [];
   constructor(
-    
+    private route: ActivatedRoute,
+    private projectService: ProjectService,
+    private router: Router
     ) {}
 
   ngOnInit() {
+    this.route.params.subscribe(
+      //Get Current Project and Current Member
+      param => {
+        this.currentProject = param;
+      }
+
+  )
     var email = firebase.auth().currentUser.email;
     var db = firebase.firestore();
-    db.collection("messages").where("recipient",'==',email).get().then((snapshot) =>{snapshot.docs.forEach(doc => {
+    db.collection("messages").where("meeting",'==',this.currentProject.id).get().then((snapshot) =>{snapshot.docs.forEach(doc => {
       var item = doc.data();
       
       //Add Task to Project in Project Service
@@ -27,13 +40,17 @@ export class MessagesPage implements OnInit {
     
   }
 
-  upload(message, recipient) {
+  goBack() {
+    this.router.navigate(["project-detail",this.currentProject])
+  }
+
+  upload(message) {
     var db = firebase.firestore();
     var user = firebase.auth().currentUser.email;
     db.collection("messages").add({
-      'recipient':recipient,
       'sender':user,
       "message":message,
+      'meeting':this.currentProject.id,
     })
     .then(function(docRef) {
        console.log("Document written with ID: ", docRef.id);
